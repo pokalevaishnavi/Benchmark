6fba932a92ab9a276e2afc7da7c51ea70917bcab
pokalevaishnavi <vaishnavpokale21@gmail.com>
2025-06-30 16:50:45 +0530

    added dynamic db to db_config

--- healthcare_bot/db_config.py
+++ healthcare_bot/db_config.py
@@ -1,6 +1,9 @@
 config = {
-    'host': 'localhost',
-    'user': 'root', 
-    'password': 'Kashala hava password@123', 
-    'database': 'healthcare_bot'
+    'host': 'b0v5y7ksi8cj9wppniyw-mysql.services.clever-cloud.com',
+    'user': 'rucah37ymw99vjgm9oot', 
+    'password': 'ucah37ymw99vjgm9', 
+    'database': 'b0v5y7ksi8cj9wppniyw',
+    'port' : '3306',
+    'Connection URI' :'Connection URI',
+    'MySQL CLI' : 'mysql -h b0v5y7ksi8cj9wppniyw-mysql.services.clever-cloud.com -P 3306 -u ucah37ymw99vjgm9 -p b0v5y7ksi8cj9wppniyw'
 }
