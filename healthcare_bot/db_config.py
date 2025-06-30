1b1c370420c1a26dec52d3c6b2fa667b330ae716
pokalevaishnavi <vaishnavpokale21@gmail.com>
2025-06-30 19:03:24 +0530

    Final webhook and db config update

--- healthcare_bot/db_config.py
+++ healthcare_bot/db_config.py
@@ -1,9 +1,9 @@
 config = {
     'host': 'b0v5y7ksi8cj9wppniyw-mysql.services.clever-cloud.com',
-    'user': 'rucah37ymw99vjgm9oot', 
-    'password': 'ucah37ymw99vjgm9', 
+    'user': 'ucah37ymw99vjgm9', 
+    'password': 'SDh9HcvujkvFKko8Ua7e', 
     'database': 'b0v5y7ksi8cj9wppniyw',
     'port' : '3306',
-    'Connection URI' :'Connection URI',
+    'Connection URI' :'mysql://ucah37ymw99vjgm9:SDh9HcvujkvFKko8Ua7e@b0v5y7ksi8cj9wppniyw-mysql.services.clever-cloud.com:3306/b0v5y7ksi8cj9wppniyw',
     'MySQL CLI' : 'mysql -h b0v5y7ksi8cj9wppniyw-mysql.services.clever-cloud.com -P 3306 -u ucah37ymw99vjgm9 -p b0v5y7ksi8cj9wppniyw'
 }
